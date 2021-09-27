09f21ed0499db8a507c040c1b0600901488dcfb7
Lucas Schoubye <lucasschoubye@gmail.com>
2021-09-27 16:42:03 +0200

    Initializere Variabler/attributer til Game Class

--- src/Game.java
+++ src/Game.java
@@ -1,6 +1,6 @@
 public class Game {
     public static void main(String[] args) {
-        // Initialize Variables/Attributes
+        // Initialize Variables/Attributes.
         int TurnIndicator=0;
         int Sum=0;
     }
