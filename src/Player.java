2de5485c90bbdc518c180462c1d16b30c7ee55f4
Lucas Schoubye <lucasschoubye@gmail.com>
2021-09-28 16:00:20 +0200

    Lille fix i player metoder

--- src/Player.java
+++ src/Player.java
@@ -1,10 +1,11 @@
 public class Player {
     int score = 0;
     int sum = 0;
-    int IncreaseScore() {
+    private void IncreaseScore() {
         score++;
-        int GetScore() {
-            return score;
-        }
     }
+    int GetScore() {
+        return score;
+    }
+
 }
