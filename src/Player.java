4e87e388e7c2a498f35182de643295b70cd86c74
Ismail Ali <s190201@student.dtu.dk>
2021-09-28 15:31:38 +0200

    Player class færdiggjordt

---
+++ src/Player.java
@@ -0,0 +1,11 @@
+public class Player {
+    int score = 0;
+    int sum = 0;
+
+    void IncreaseScore() {
+        score++;
+        int GetScore() {
+            return score;
+        }
+    }
+}
