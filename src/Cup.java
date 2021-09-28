a04904b69c7d3d7aa9d4471969cb2cfd67119fd7
Mads <kontakt@mswebpartner.dk>
2021-09-28 14:52:17 +0200

    Tilføjelse af constructor til Cup, og GetSum() funktion

--- src/Cup.java
+++ src/Cup.java
@@ -5,4 +5,17 @@ public class Cup {
     int CreatedDice=0;
     int Sum=0;
 
+    // Create Die Objects
+    public Cup {
+        Die Die1 = new Die();
+        Die Die2 = new Die();
+
+        Die1.RollDie();
+        Die2.RollDie();
+    }
+
+    int GetSum(){
+        Sum=Die1.FaceValue+Die2.FaceValue;
+        return Sum;
+    }
 }
