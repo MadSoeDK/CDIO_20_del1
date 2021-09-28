d319755a2080a2cc4100e54720676b6311eabadc
Mads <kontakt@mswebpartner.dk>
2021-09-28 15:16:45 +0200

    Ændring af constructor

--- src/Cup.java
+++ src/Cup.java
@@ -1,21 +1,20 @@
 public class Cup {
     // Initialize Variables
-    final int QUANTITY=2;
-
-    int CreatedDice=0;
-    int Sum=0;
+    private final int QUANTITY=2;
+    private int sum;
 
     // Create Die Objects
     public Cup {
         Die Die1 = new Die();
         Die Die2 = new Die();
 
-        Die1.RollDie();
-        Die2.RollDie();
+        Die1.roll();
+        Die2.roll();
     }
 
     int GetSum(){
-        Sum=Die1.FaceValue+Die2.FaceValue;
-        return Sum;
+        sum = facevalue1 + facevalue2;
+        return sum;
     }
 }
+
