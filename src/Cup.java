8036232eff983c964930b5a4b1f2ca616753606c
Mads <kontakt@mswebpartner.dk>
2021-09-28 14:50:52 +0200

    Initialiserings af variabler

---
+++ src/Cup.java
@@ -0,0 +1,8 @@
+public class Cup {
+    // Initialize Variables
+    final int QUANTITY=2;
+
+    int CreatedDice=0;
+    int Sum=0;
+
+}
--- src/Raflecup.java
+++
@@ -1,5 +0,0 @@
-public class Raflecup {
-    public static void main(String[] args) {
-        
-    }
-}
